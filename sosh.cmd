a7aab3d214c2fd44408f9f15d19e3633c8ef90b0
magomed <m@shamhalov.ru>
2025-10-12 09:58:04 +0300

    sosh interpreter

--- sosh.cmd
+++ sosh.cmd
@@ -1,1 +1,5 @@
-node ./sosh/sosh
+# Please copy this file to C:\Users\<username>\AppData\Local\Microsoft\WindowsApps
+# for use simple sosh command to run this app
+
+cd C:/dev/soagent/
+bun ./sosh/sosh
